a358ea6ba9d6205487325e4d77031d3660dbf84d
Hager706 <hagertarek706@gmail.com>
2026-01-21 05:35:20 +0200

    test changes

--- src/app.js
+++ src/app.js
@@ -6,8 +6,8 @@ return "Hello, " + name + "!";
 
 module.exports = greet; 
 
-if (require.main === module) {
+// if (require.main === module) {
 
-console.log(greet("World"));
+// console.log(greet("World"));
 
-}
+// }
